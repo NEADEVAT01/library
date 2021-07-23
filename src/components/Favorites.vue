357381d9f55ece10639a57aa3d5c1cc592315d65
NEADEVAT01 <pavel.melnik.2001@hotmail.com>
2021-07-23 18:16:12 +0700

    finished

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,14 +1,50 @@
 <template>
-  <h1>Favoriter</h1>
-  <div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
+  <div class="container-fluid px-5">
+    <h1 class="text-center m-5 display-3">Избранное</h1>
+    <div class="row" v-if="data != []">
+      <div class="col-lg-3 col-sm-12 border" v-for="data in data" :key=data.id>
+        <img :src="data.cover_url" alt="cover" class="w-50 rounded mx-auto my-2 d-block">
+        <p class="h5 my-2">{{ data.name }}</p>
+        <p class="my-1"><span class="text-muted">Автор: </span>{{ data.author }}</p>
+        <p class="my-1"><span class="text-muted">Издательство: </span>{{ data.publisher }}</p>
+        <p class="my-1"><span class="text-muted">Год издания: </span>{{ data.publication_date }}</p>
+        <p class="my-1"><span class="text-muted">Рейтинг: </span><button class="btn btn-outline-success"
+            v-on:click="Rating(1,data.id)">+</button> {{ data.rating }}
+          <button class="btn btn-outline-danger" v-on:click="Rating(-1,data.id)">-</button></p>
+        <button class="my-2 btn btn-outline-danger" v-on:click="deleteFavorite(data.id)">Удалить из избранного</button>
+      </div>
+    </div>
+    <div v-if="data = []" class="text-center">
+    <p class="text-muted display-4">К сожелению вы не добавили ничего в избранное :(</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'favorites',  
-}
+  export default {
+    name: 'favorites',
+    data() {
+      return {
+        data: []
+      }
+    },
+    created() {
+      if (localStorage.favorites != null) {
+        this.data = JSON.parse(localStorage.favorites)
+        this.data = this.data.filter(a => a != null)
+      }
+    },
+    methods: {
+      Rating(score, id) {
+        this.data[this.data.findIndex(el => el.id === id)].rating += score
+      },
+      deleteFavorite(id) {
+        this.data.splice(this.data.findIndex(el => el.id === id), 1)
+        localStorage.favorites = JSON.stringify(this.data)
+        return 0;
+      },
+    }
+  }
 </script>
 
 <style scoped>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="home">
     <div class="container-fluid px-5">
-      <h1 class="text-center m-5">Наши книги</h1>
+      <h1 class="text-center m-5 display-3">Наши книги</h1>
       <div class="text-center mb-3">
         <div>
           <p>Сортировка</p>
-          <button v-on:click="Sort_year_desc" class="mx-1">Год ▼</button>
-          <button v-on:click="Sort_year_asc" class="mx-1">Год ▲</button>
-          <button v-on:click="Sort_name_desc" class="mx-1">Название ▼</button>
-          <button v-on:click="Sort_name_asc" class="mx-1">Название ▲</button>
-          <button v-on:click="Sort_rating_desc" class="mx-1">Рейтинг ▼</button>
-          <button v-on:click="Sort_rating_asc" class="mx-1">Рейтинг ▲</button>
+          <button v-on:click="Sort_year_desc" class="mx-1 btn btn-outline-secondary">Год ▼</button>
+          <button v-on:click="Sort_year_asc" class="mx-1 btn btn-outline-secondary">Год ▲</button>
+          <button v-on:click="Sort_name_desc" class="mx-1 btn btn-outline-secondary">Название ▼</button>
+          <button v-on:click="Sort_name_asc" class="mx-1 btn btn-outline-secondary">Название ▲</button>
+          <button v-on:click="Sort_rating_desc" class="mx-1 btn btn-outline-secondary">Рейтинг ▼</button>
+          <button v-on:click="Sort_rating_asc" class="mx-1 btn btn-outline-secondary">Рейтинг ▲</button>
         </div>
         <div class="my-3">
           <p>Фильтры</p>
-          <select v-model="filter.publisher" class="mx-2">
-            <option v-for="publisher in publishers" :key=publisher>{{publisher}}</option>
-          </select>
-          <select v-model="filter.author" class="mx-2">
-            <option v-for="author in authors" :key=author>{{author}}</option>
-          </select>
-          <select v-model="filter.year" class="mx-2">
-            <option v-for="year in years" :key=year>{{year}}</option>
-          </select>
-          <button v-on:click="Filter">Поиск</button>
+          <div class="d-flex justify-content-center">
+            <select v-model="filter.publisher" class="m-2 form-select w-25">
+              <option v-for="publisher in publishers" :key=publisher>{{publisher}}</option>
+            </select>
+            <select v-model="filter.author" class="m-2 form-select w-25">
+              <option v-for="author in authors" :key=author>{{author}}</option>
+            </select>
+            <select v-model="filter.year" class="m-2 form-select w-25">
+              <option v-for="year in years" :key=year>{{year}}</option>
+            </select>
+            <button v-on:click="Filter" class="btn btn-outline-primary">Поиск</button>
+          </div>
+
         </div>
-        <button v-on:click="Default" v-if="hasFilters" class="mx-1">✖</button>
         <div class="d-flex justify-content-center my-3">
           <input class="form-control me-2 w-25" type="search" placeholder="Поиск по названию" aria-label="Search"
             v-model="searchRequest" v-on:keyup.enter="Search">
           <button class="btn btn-outline-primary" v-on:click="Search">Поиск</button>
         </div>
+        <button v-on:click="Default" v-if="hasFilters" class="mx-1 btn btn-outline-danger">Сброс</button>
       </div>
       <div class="row" v-if="!search">
         <div class="col-lg-3 col-sm-12 border" v-for="data in data" :key=data.id>
@@ -39,9 +42,10 @@
           <p class="my-1"><span class="text-muted">Автор: </span>{{ data.author }}</p>
           <p class="my-1"><span class="text-muted">Издательство: </span>{{ data.publisher }}</p>
           <p class="my-1"><span class="text-muted">Год издания: </span>{{ data.publication_date }}</p>
-          <p class="my-1"><span class="text-muted">Рейтинг: </span><button>+</button> {{ data.rating }}
-            <button>-</button></p>
-          <button class="my-2">В Избранное</button>
+          <p class="my-1"><span class="text-muted">Рейтинг: </span><button class="btn btn-outline-success"
+              v-on:click="Rating(1,data.id)">+</button> {{ data.rating }}
+            <button class="btn btn-outline-danger" v-on:click="Rating(-1,data.id)">-</button></p>
+          <button class="my-2 btn btn-outline-success" v-on:click="addToFavorite(data.id)">♡ В Избранное</button>
         </div>
       </div>
       <div class="row" v-if="search">
@@ -51,9 +55,11 @@
           <p class="my-1"><span class="text-muted">Автор: </span>{{ searchResult.author }}</p>
           <p class="my-1"><span class="text-muted">Издательство: </span>{{ searchResult.publisher }}</p>
           <p class="my-1"><span class="text-muted">Год издания: </span>{{ searchResult.publication_date }}</p>
-          <p class="my-1"><span class="text-muted">Рейтинг: </span><button>+</button> {{ searchResult.rating }}
-            <button>-</button></p>
-          <button class="my-2">В Избранное</button>
+          <p class="my-1"><span class="text-muted">Рейтинг: </span><button class="btn btn-outline-success"
+              v-on:click="Rating(1,searchResult.id)">+</button> {{ searchResult.rating }}
+            <button class="btn btn-outline-danger" v-on:click="Rating(-1,searchResult.id)">-</button></p>
+          <button class="my-2 btn btn-outline-success" v-on:click="addToFavorite(searchResult.id)">♡ В
+            Избранное</button>
         </div>
       </div>
     </div>
@@ -73,14 +79,17 @@
         years: [],
         publishers: [],
         authors: [],
-        filter: []
+        filter: [],
+        favorites: [],
       }
     },
     created() {
       fetch("books.json")
         .then(response => response.json())
         .then(data => (this.data = data));
-
+      if (localStorage.favorites != null) {
+        this.favorites = JSON.parse(localStorage.favorites)
+      }
     },
     beforeUpdate() {
       this.years = this.data.map(item => item.publication_date).sort((a, b) => a - b).filter((val, i, ar) => ar.indexOf(
@@ -143,19 +152,21 @@
         }
       },
       Filter() {
-         this.searchResult = this.data
-        if  (this.filter.publisher != null) {
+        this.searchResult = this.data
+        if (this.filter.publisher != null) {
           this.searchResult = this.searchResult.filter((a) => a.publisher.includes(this.filter.publisher))
-        } 
-        if  (this.filter.author != null) {
+        }
+        if (this.filter.author != null) {
           this.searchResult = this.searchResult.filter((a) => a.author.includes(this.filter.author))
-        } 
-        if  (this.filter.year != null) {
+        }
+        if (this.filter.year != null) {
           this.searchResult = this.searchResult.filter((a) => a.publication_date.includes(this.filter.year))
-        } 
-
+        }
         this.search = true
         this.hasFilters = true
+      },
+      Rating(score, id) {
+        this.data[id].rating += score
       },
       Default() {
         fetch("books.json")
@@ -165,7 +176,14 @@
         this.hasFilters = false
         this.search = false
         this.filter = []
-      }
+      },
+      addToFavorite(id) {
+        if (this.favorites[id] != this.data[id]) {
+          this.favorites[id] = this.data[id]
+          localStorage.favorites = JSON.stringify(this.favorites)
+          return 0
+        }
+      },
     }
   }
 </script>
